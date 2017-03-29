b0bcfaf82f79d59cf65cf1de20b1cdd7e6994b5c
gustafwesterholm <gustafwest@gmail.com>
2017-03-29 14:45:40 +0200

    Grip arm for 128 kHz done

--- PWM_grip_arm.c
+++ PWM_grip_arm.c
@@ -5,32 +5,46 @@
  *  Author: guswe541
  */ 
 
+#define F_CPU 128000UL
 
 #include <avr/io.h>
-//#include <util/delay.h>
+#include <util/delay.h>
+#include "PWM_grip_arm.h"
 
 void Timer2_init()
 {
 	TCCR2A |= (1<<WGM20)|(1<<COM2B1)|(1<<WGM21); // |(1<<COM2A1); fast PWM-mode, MAX = TOP
-	TCCR2B |= (1<<CS21) | (1 << CS20); //clock select 32 prescaler (Change later)
+	TCCR2B |= (1<<CS21); // clock select 8 prescaler for 128000 kHz
 }
 
-void Grip_power(float grip) // controls the grip power. 0 < grip < 1.
+void Open_grip_arm()
 {
-	OCR2B = grip*255;
+	OCR2B = 30; // 2 ms with 128000 kHz
+}
+
+void Center_grip_arm()
+{
+	OCR2B = 22; // 1.5 ms with 128000 kHz
+}
+
+void Close_grip_arm()
+{
+	OCR2B = 15; //15 1 ms with 128000 kHz
 }
 
 int main()
 {
-	DDRD |= (1<<PORTD6); // |(1<<PORTD7); // OSC2A and OSC2B as outputs.
-
+	DDRD |= (1<<PORTD6); // OSC2A and OSC2B as outputs.
+	CLKPR |= (1<<CLKPCE); // prescaler enabled
 	Timer2_init(); // Timer initialization.
 	
-	Grip_power(0.02);
-	//OCR2A = 127; PWM for LIDAR
-
+	Center_grip_arm();
 	while(1)
 	{
-		// Infinite loop.
+		// Infinite loop.		
+		Open_grip_arm();
+		_delay_ms(2000);
+		Close_grip_arm();
+		_delay_ms(2000);	
 	}
 }
---
+++ PWM_grip_arm.h
@@ -0,0 +1,12 @@
+#ifndef PWM_GRIP_ARM_H
+#define PWM_GRIP_ARM_H
+
+void Timer2_init();
+
+void Open_grip_arm();
+
+void Center_grip_arm();
+
+void Close_grip_arm();
+
+#endif
