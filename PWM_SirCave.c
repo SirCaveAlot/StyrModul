02b7c72526bb3b73c02f1a0508b3fbbe83322408
StrBerg <gusst967@student.liu.se>
2017-03-31 00:11:38 +0200

    Reads two bytes to OCR1 instead of one word

--- PWM_SirCave.c
+++ PWM_SirCave.c
@@ -9,25 +9,34 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "PWM_SirCave.h"
 //#include <avr/interrupt.h>
 
 void Timer1_init()
 {
-	CLKPCR |= (1 << CLKPCE); // Prescaler enabled.
-	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11);  // Intern clock, prescaler 8.
-	TCCR1A |= (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1); // set fast PWM-mode and non-inverted mode
+	CLKPR |= (1 << CLKPCE); // Prescaler enabled.
+	TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11);  // Fast PWM, ICR1 = TOP, prescaler 8.
+	TCCR1A |= (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1); // set fast PWM-mode, ICR1 = TOP, non-inverting mode
 	
 	ICR1 = 36863; // Top-value PWM. (2559 128kHz)
 }
 
 void Set_speed_right(float velocity_right)
 {
-	OCR1B = velocity_right*ICR1;
+	uint16_t speed_data = velocity_right*ICR1;
+	uint8_t LSB_speed_data = speed_data;
+	uint8_t MSB_speed_data = speed_data >> 8;	
+	OCR1BL = LSB_speed_data;
+	OCR1BH = MSB_speed_data;
 }
 
 void Set_speed_left(float velocity_left)
 {
-	OCR1A = velocity_left*ICR1;
+	uint16_t speed_data = velocity_left*ICR1;
+	uint8_t LSB_speed_data = speed_data;
+	uint8_t MSB_speed_data = speed_data >> 8;
+	OCR1AL = LSB_speed_data;
+	OCR1AH = MSB_speed_data;
 }
 
 void Drive_forward(float velocity_left, float velocity_right)
@@ -61,20 +70,21 @@ void Rotate_counter_clockwise(float velocity_left, float velocity_right)
 int main(void)
 {
 	DDRD = 0b11000000;
-	DDRA = 0xFF;	
+	DDRA = 0b00000011;	
 	
 	Timer1_init();
 	
+	
     while(1)
     {
-        Drive_forward(0.7, 0.7);
-		_delay_ms(1000);
+        Drive_forward(0.5, 0.5);
+		_delay_ms(1);
 		Drive_backwards(0.9, 0.9);
-		_delay_ms(1000);
+		_delay_ms(1);
 		Rotate_clockwise(0.5, 0.5);
-		_delay_ms(1000);
+		_delay_ms(1);
 		Rotate_counter_clockwise(0.8, 0.8);
-		_delay_ms(1000);
+		_delay_ms(1);
 		//TODO:: Please write your application code 
     }
 }
