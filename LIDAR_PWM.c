64e011fb40d7557ec2eb3b0696a1c9078de0720e
gustafwesterholm <gustafwest@gmail.com>
2017-04-10 16:15:50 +0200

    lidar pwm

--- LIDAR_PWM.c
+++ LIDAR_PWM.c
@@ -6,12 +6,21 @@
  */ 
 
 #define F_CPU 14745600UL
+
 #include <avr/io.h>
+#include <util/delay.h>
 #include "PWM_SirCave.h"
+
+/*
+void Timer2_init()
+{
+	TCCR2A |= (1 << WGM20) | (1 << COM2B1) | (1 << WGM21) | (1 << COM2A1); //fast PWM-mode, MAX = TOP
+	TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20); // clock select 8 prescaler for 128000 kHz
+}*/
 
 void LIDAR_on()
 {
-	OCR2A = 0.95*ICR1;
+	OCR2A = 128;
 }
 
 void LIDAR_off()
@@ -21,8 +30,16 @@ void LIDAR_off()
 
 int main(void)
 {
+	Timer2_init();
+	DDRD |= (1<<PORTD6)|(1<<PORTD7); // OC2A  output.
+	//OCR2A = 50;
+	//OCR2B = 50;
     while(1)
     {
+		LIDAR_on();
+		_delay_ms(1000);
+		LIDAR_off();
+		_delay_ms(1000);
         //TODO:: Please write your application code 
     }
 }
